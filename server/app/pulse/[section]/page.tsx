60a9fa55f5a001ce0061020b9c1a4f93922b002c
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2024-10-04 19:40:49 +0530

    Added info about how to connect to web-preview.

--- server/app/pulse/[section]/page.tsx
+++ server/app/pulse/[section]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UIAgentContext, useAppInfo, useComponentTree, useConsole, useNetworkRequests, usePlatformInfo, useStorageEntries, useTimelineLog } from "@/hooks/hooks";
-import { Tabs, Tab, Card, CardBody, Button, Input } from "@nextui-org/react";
+import { Tabs, Tab, Card, CardBody, Button, Input, DropdownMenu, DropdownItem, Dropdown, DropdownTrigger, ButtonGroup } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { useContext, useState, useEffect , useMemo, useCallback} from "react";
 import { Info } from "./info";
@@ -16,6 +16,18 @@ import { WidgetNode } from "@wavemaker/wavepulse-agent/src/types";
 import {BreadcrumbsComponent} from "@/components/breadcrumbs";
 import { TimeLine } from "./timeline";
 import QRCode from "react-qr-code";
+import { ChevronDownIcon } from "@nextui-org/shared-icons";
+
+const connectOptions = {
+  mobile: {
+    label: 'Connect to APK or IPA',
+    description: 'Connect to a WaveMaker React Native APK or IPA.'
+  },
+  webpreview: {
+    label: 'Connect to Web Preview',
+    description: 'Connect to a WaveMaker React Native web preview.'
+  }
+};
 
 export default function PulsePage({ params }: { params: { section: string } } ) {
 
@@ -36,6 +48,7 @@ export default function PulsePage({ params }: { params: { section: string } } ) 
   const [breadcrumbData, setBreadcrumbData]=useState<WidgetNode[]>();
   const [appId, setAppId] = useState(localStorage.getItem('wavepulse.lastopenedapp.id') || 'com.application.id');   //should get props.path into this page
   const [url, setUrl] = useState('');
+  const [selectedConnectOption, setSelectedConnectOption] = useState(['mobile']);
   useEffect(() => {
     appId && uiAgent.getWavepulseUrl(appId).then(url => setUrl(url));
     localStorage.setItem('wavepulse.lastopenedapp.id', appId);
@@ -104,29 +117,77 @@ export default function PulsePage({ params }: { params: { section: string } } ) 
         </Tabs>) : 
         null }
         {isConnected ? null : (
-          <div className="flex flex-1 flex-col justify-center content-center items-center flex-wrap">
+          <div 
+            className="flex flex-1 flex-col justify-center content-center items-center flex-wrap">
             <div className="lds-ripple"><div></div><div></div></div>
-            <div className="p-2 text-sm" style={{width: 400}}>
-              <div className="text-sm text-gray-400 ">{"1) Enter the Application Id below. Application Id is available in Export React Zip dialog in Studio."}</div>
-            </div>
-            <div className="p-2" style={{width: 400}}>
-              <Input type="text" defaultValue={appId} className="w-full" placeholder="Ex: com.application.id" onChange={(event) => setAppId(event.target.value)}/>
-            </div>
-            <div className="p-2 text-sm" style={{width: 400}}>
-              <div className="text-sm text-gray-400">{"2) Using your phone, scan the below QR code, which contains url."}</div>
-            </div>
-            <div className="p-2">
-              <QRCode value={url || ''}/>
-            </div>
-            <div className="p-2">
-              <a className="text-sm break-all w-full underline text-center" href={url}>Copy this link</a>
-            </div>
-            <div className="p-2" style={{width: 400}}>
-              <div className="text-sm text-gray-400">{"3) When the url is opened in phone web browser, App that has the above application id will be opened."}</div>
-            </div>
-            {/* <div className="p-2" style={{width: 400}}>
-              <div className="text-sm">{"4) Message is shown when the app is connected to WavePulse."}</div>
-            </div> */}
+                <ButtonGroup>
+                  <Button 
+                    style={{width: 360}}
+                    variant="bordered">
+                      {(connectOptions as any)[selectedConnectOption[0]].label}
+                  </Button>
+                <Dropdown>
+                  <DropdownTrigger>
+                      <Button isIconOnly>
+                        <ChevronDownIcon />
+                      </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    disallowEmptySelection
+                    selectedKeys={selectedConnectOption}
+                    selectionMode="single"
+                    className="max-w-[300px]"
+                    onSelectionChange={(keys) => {
+                      setSelectedConnectOption([keys.currentKey || '']);
+                    }}
+                  >
+                  <DropdownItem key="mobile" description={connectOptions.mobile.description}>
+                    {connectOptions.mobile.label}
+                  </DropdownItem>
+                  <DropdownItem key="webpreview" description={connectOptions.webpreview.description}>
+                    {connectOptions.webpreview.label}
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </ButtonGroup>
+            {
+              selectedConnectOption[0] === 'mobile' ? (<div style={{minHeight: 600}}
+                className="flex flex-col content-center items-center flex-wrap">
+                <div className="p-2 text-sm" style={{width: 400}}>
+                  <div className="text-sm text-gray-400 ">{"1) Enter the Application Id below. Application Id is available in Export React Zip dialog in Studio."}</div>
+                </div>
+                <div className="p-2" style={{width: 400}}>
+                  <Input type="text" defaultValue={appId} className="w-full" placeholder="Ex: com.application.id" onChange={(event) => setAppId(event.target.value)}/>
+                </div>
+                <div className="p-2 text-sm" style={{width: 400}}>
+                  <div className="text-sm text-gray-400">{"2) Using your phone, scan the below QR code, which contains url."}</div>
+                </div>
+                <div className="p-2">
+                  <QRCode value={url || ''}/>
+                </div>
+                <div className="p-2">
+                  <a className="text-sm break-all w-full underline text-center" href={url}>Copy this link</a>
+                </div>
+                <div className="p-2" style={{width: 400}}>
+                  <div className="text-sm text-gray-400">{"3) When the url is opened in phone web browser, App that has the above application id will be opened."}</div>
+                </div>
+                {/* <div className="p-2" style={{width: 400}}>
+                  <div className="text-sm">{"4) Message is shown when the app is connected to WavePulse."}</div>
+                </div> */}
+              </div>) : null
+            }
+            {
+              selectedConnectOption[0] === 'webpreview' ? (<div style={{minHeight: 600}} 
+                className="flex flex-col content-center items-center flex-wrap">
+                <div className="p-2 text-sm" style={{width: 400}}>
+                  <div className="text-sm text-gray-400 ">{"1) In the browser tab where app is running, open developer console."}</div>
+                </div>
+                <div className="p-2 text-sm" style={{width: 400}}>
+                  <div className="text-sm text-gray-400">{"2) Execute the below code in the developer console."}</div>
+                </div>
+                <pre className="text-black text-sm">{`wm.App.tryToconnectWavepulse('${location.origin}');`}</pre>
+              </div>) : null
+            }
           </div>
         )}
       <SaveDataDialog isOpen={isSaveDataOpened} onClose={() => setIsSaveDataOpen(false)}></SaveDataDialog>
