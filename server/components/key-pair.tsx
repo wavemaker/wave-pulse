11262176680384a30706f61271a6047e8a2f1573
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2024-10-22 10:10:13 +0530

    Fixed Issues wit storage and info.

--- server/components/key-pair.tsx
+++ server/components/key-pair.tsx
@@ -11,7 +11,7 @@ export const KeyValuePair = (props: KeyValueProps) => {
         return (
             <> {
             props.entries.map(e => (
-                <div className="flex flex-row border-b">
+                <div className="flex flex-row border-b hover:bg-slate-200">
                     <div className="flex-shrink-0 text-xs font-bold w-4/12 px-4 py-1">{e.key}</div>
                     <div className="text-xs  py-1">{e.value === undefined ? '' : e.value}</div>
                 </div>
