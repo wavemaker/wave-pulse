90c387594ff2ebf5c58e17e1f6e6bf194a778aa2
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2024-10-24 10:20:16 +0530

    Add appId in the page

--- server/app/connect/page.tsx
+++ server/app/connect/page.tsx
@@ -7,13 +7,13 @@ import { Suspense, useCallback, useEffect, useState } from "react";
 export function ConnectPage() {
     const searchParams = useSearchParams();
     const [time, setTime] = useState(5);
+    const launchUrl = decodeURIComponent(searchParams.get('launchUrl') || '').trim();
     const launch = useCallback(() => {
-        const launchUrl = decodeURIComponent(searchParams.get('launchUrl') || '').trim();
         if (!launchUrl.startsWith('http')
             && !launchUrl.startsWith('://')) {
             window.location.href= launchUrl;
         }
-    }, [searchParams.get('launchUrl')]);
+    }, [launchUrl]);
     useEffect(() => {
         if (time > 0) {
             setTimeout(() => {
@@ -26,20 +26,31 @@ export function ConnectPage() {
             launch();
         }
     }, [time, launch]);
+    const appId = launchUrl.split('://')[0];
     return (
-    <div className='flex flex-col justify-center items-center h-svh'>
+    <div className='flex flex-col justify-center items-center h-svh p-4'>
         {
             time > 0 ? (
+                <>
                 <div className='text-sm'>
                     App should be automatically opened in <span className='font-bold text-large text-red-600'>{time}</span> seconds
                 </div>
+
+                <div className='text-sm py-1 text-center'>
+                    <span>App Id:</span> <span className='font-bold'>{appId}</span>
+                </div>
+                </>
             ) : null
         }
         {
             time > 0 ? null : (
                 <>
-                <div className='text-sm py-4'>
+                <div className='text-sm py-1 text-center'>
                     If the app is not opened yet, please click on the below button.
+                </div>
+
+                <div className='text-sm py-1 text-center'>
+                    <span className='italic'>App Id:</span> <span className='font-bold'>{appId}</span>
                 </div>
                 <Button 
                     className='text-sm bg-green-900 px-4 text-white'
